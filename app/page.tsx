64ee81ebf50c875a855da57a17d5f789723c05e3
Karthik Manimaran <kmanimaran@heartflow.com>
2026-03-02 17:50:59 -0800

    Fix TypeScript errors and add Dockerfile for Kubernetes deployment

--- app/page.tsx
+++ app/page.tsx
@@ -35,34 +35,38 @@ export default function Home() {
         {/* Tabs for Categories */}
         <TabGroup className="mb-8">
           <TabList className="mb-6">
-            <Tab>All Categories</Tab>
-            {categories.map((category) => (
-              <Tab key={category.id}>
-                <span className="mr-2">{category.icon}</span>
-                {category.name.split(' ')[0]}
-                <span className="ml-2 text-xs bg-gray-200 px-2 py-0.5 rounded-full">
-                  {category.metrics.length}
-                </span>
-              </Tab>
-            ))}
+            <>
+              <Tab>All Categories</Tab>
+              {categories.map((category) => (
+                <Tab key={category.id}>
+                  <span className="mr-2">{category.icon}</span>
+                  {category.name.split(' ')[0]}
+                  <span className="ml-2 text-xs bg-gray-200 px-2 py-0.5 rounded-full">
+                    {category.metrics.length}
+                  </span>
+                </Tab>
+              ))}
+            </>
           </TabList>
 
           <TabPanels>
-            {/* All Categories View */}
-            <TabPanel>
-              <div className="space-y-8">
-                {filteredCategories.map((category) => (
-                  <CategorySection key={category.id} category={category} />
-                ))}
-              </div>
-            </TabPanel>
-
-            {/* Individual Category Views */}
-            {categories.map((category) => (
-              <TabPanel key={category.id}>
-                <CategorySection category={category} />
+            <>
+              {/* All Categories View */}
+              <TabPanel>
+                <div className="space-y-8">
+                  {filteredCategories.map((category) => (
+                    <CategorySection key={category.id} category={category} />
+                  ))}
+                </div>
               </TabPanel>
-            ))}
+
+              {/* Individual Category Views */}
+              {categories.map((category) => (
+                <TabPanel key={category.id}>
+                  <CategorySection category={category} />
+                </TabPanel>
+              ))}
+            </>
           </TabPanels>
         </TabGroup>
 
